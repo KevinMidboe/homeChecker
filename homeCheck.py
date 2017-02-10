0318202aa556a6c0aaa415e0acf7b94a6c9955d2
KevinMidboe <kevin.midboe@gmail.com>
2017-02-10 23:36:16 +0100

    Added so you can add to cmd prompt if you want to filter out all other names than the name specified.

--- homeCheck.py
+++ homeCheck.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from socket import *
-from pprint import pprint
 import json
+from sys import argv
 
 # Set host name and port
 host = '10.0.0.41'
@@ -10,13 +10,21 @@ port = 12001
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-def main():
+def fetchHTimes(filter=None):
     clientSocket.sendto(b'get_times', (host, port))
     message, address = clientSocket.recvfrom(1024)
     message = message.decode('utf-8')
+    
     for item in json.loads(message):
-        print(item['name'].ljust(10) + ': ' + item['time'])
+        if filter != None and item['name'].lower() == filter:
+            print(item['name'].ljust(10) +': '+ item['time'])
+            break
+        elif filter == None:
+            print(item['name'].ljust(10) +': '+ item['time'])
     clientSocket.close()
 
 if __name__ == '__main__':
-    main()
+    if argv[-1][-3:] == '.py':
+        fetchHTimes()
+    else:
+        fetchHTimes(argv[-1])
